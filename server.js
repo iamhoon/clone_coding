a56aa48a9e152ec912190ee7350f0a6d893b879a
iamhoon <zown98@naver.com>
2020-09-03 16:39:34 +0900

    implement customer add module & customer list refresh module

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest:'./upload'});
+
 // app.get('/api/hello', (req, res) => {
 //     res.send({message: 'Hello Express!'});
 // });
@@ -57,6 +60,23 @@ app.get('/api/customers', (req, res) => {
     //         'job': '대학생'
     //       }
     // ]);
+})
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params,
+        (err, row, fields) => {
+            res.send(rows);
+        }
+    );
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
